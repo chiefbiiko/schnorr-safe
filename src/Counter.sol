c5f27644c099431cb06d9c0a045feb19624de5eb
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-05-24 12:53:35 +0200

    wip: setup

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+interface GnosisSafe {
+        enum Operation {
+        Call,
+        DelegateCall
     }
+    
+    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
+    /// @param to Destination address of module transaction.
+    /// @param value Ether value of module transaction.
+    /// @param data Data payload of module transaction.
+    /// @param operation Operation type of module transaction.
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation
+    ) external returns (bool success);
+}
 
-    function increment() public {
-        number++;
-    }
+contract SchnorrModule {
+
+    //  require(safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
 }
