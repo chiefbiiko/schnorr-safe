208fa3f0854e3e113e82806cf351437cb9b569de
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-05-24 13:25:41 +0200

    wip

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,26 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface GnosisSafe {
-        enum Operation {
-        Call,
-        DelegateCall
-    }
-    
-    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
-    /// @param to Destination address of module transaction.
-    /// @param value Ether value of module transaction.
-    /// @param data Data payload of module transaction.
-    /// @param operation Operation type of module transaction.
-    function execTransactionFromModule(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Enum.Operation operation
-    ) external returns (bool success);
+import "/lib/safe-smart-account/contracts/common/Enum.sol";
+
+interface ISafe {
+    function execTransactionFromModule(address to, uint256 value, bytes memory data, uint8 operation) external returns (bool success);
 }
 
 contract SchnorrModule {
+
+    function verifySignature(bytes sig) internal pure returns (bool) {
+
+        
+
+        return false;
+    }
 
     //  require(safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
 }
