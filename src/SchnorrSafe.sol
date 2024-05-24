711fb9dca17e6ae25350c1295a888d0f3e626ffc
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-05-24 15:09:02 +0200

    feat: dirty draft

--- src/SchnorrSafe.sol
+++ src/SchnorrSafe.sol
@@ -2,30 +2,45 @@
 pragma solidity ^0.8.13;
 
 import { Enum } from "safe-smart-account/contracts/common/Enum.sol";
+import { ModuleManager, OwnerManager } from "safe-smart-account/contracts/base/ModuleManager.sol";
 import { LibSchnorr } from "scribe/src/libs/LibSchnorr.sol";
-
-interface ISafe {
-    function execTransactionFromModule(address to, uint256 value, bytes memory data, uint8 operation) external returns (bool success);
-}
+import { LibSchnorrExtended } from "scribe/script/libs/LibSchnorrExtended.sol";
 
 contract SchnorrSafe {
+    using LibSchnorrExtended for LibSecp256k1.Point[];
 
-    function verifySignature(
-        LibSecp256k1.Point memory aggPubKey,
-        bytes32 message,
-        bytes32 signature,
-        address commitment
-    ) internal pure returns (bool valid) {
-        return LibSchnorr.verifySignature(aggPubKey, message, signature, commitment);
-    }
-
-    function exec(
+    function execute(
+        address safe,
          address to, uint256 value, bytes memory data, uint8 operation,
-        LibSecp256k1.Point memory aggPubKey,  bytes32 message,    bytes32 signature,   address commitment
+        LibSecp256k1.Point[] memory pubKeys,  bytes32 message,    bytes32 signature,   address commitment
     )  external returns (bool success) {
-        //TODO extract number of signatories from aggPubkey or whatever 0>
-        // to make sure it is gte the Safe#s threshold
-    }
+        // Make sure number of public keys is gte threshold
+        require(pubKeys.length >= OwnerManager(safe).getThreshold());
 
-    //  require(safe.execTransactionFromModule(token, 0, data, Enum.Operation.Call), "Could not execute token transfer");
+        // Make sure each pubKey belongs to a distinct Safe owner
+        address[] memory owners = OwnerManager(safe).getOwners();
+        for (uint i = 0; i < pubKeys.length; i++) {
+            address adrs =  pubKeys[i].toAddress();
+            int ownerIndex = -1;
+            for (uint j = 0; j < owners.length; j++) {
+                if (adrs == address(owners[j])) {
+                    ownerIndex = j;
+                    break;
+                }
+            }
+
+            // Must be an owner
+            require(ownerIndex != -1); //TODO proper err
+
+            // Prevents double signing
+            delete owners[ownerIndex];
+        }
+
+        // Verify Schnorr signature
+        LibSecp256k1.Point memory aggPubKey = pubKeys.aggregatePublicKeys();
+        require(LibSchnorr.verifySignature(aggPubKey, message, signature, commitment)); //TODO proper err
+
+        // Dispatch Safe transaction
+        require(ModuleManager(safe).execTransactionFromModule(to, value, data, operation)); //TODO proper err
+    }
 }
